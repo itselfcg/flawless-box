2f4954adf258c41b91151170e9ff7ec30dee4248
Itzel Contreras <itselfcg@gmail.com>
2021-03-03 10:05:13 -0800

    Laravel configuration.

---
+++ resources/views/wiki.blade.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Flawless Box | Wiki</title>
+  <link rel="stylesheet" href="css/style-wiki.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=Raleway&family=Roboto&display=swap" rel="stylesheet">
+<nav id="navbar">
+   <header>
+      <h1>
+      SKINCARE WIKI
+      <h1>
+   </header>
+   <h2><a class="link" href="{{ url('/') }}" target="_blank" >BY FLAWLESS BOX</a></h2>
+   <ul>
+      <li><a class="nav-link" href="#Introduction" >Introduction</a></li>
+      <li><a class="nav-link" href="#Oil_cleanser" >Oil cleanser</a></li>
+      <li><a class="nav-link" href="#Foam_cleanser" >Foam cleanser</a></li>
+      <li><a class="nav-link" href="#Toner" >Toner</a></li>
+      <li><a class="nav-link" href="#Essence" >Essence</a></li>
+      <li><a class="nav-link" href="#Serum" >Serum</a></li>
+      <li><a class="nav-link" href="#Eye_cream" >Eye cream</a></li>
+      <li><a class="nav-link" href="#Moisturizer" >Moisturizer</a></li>
+      <li><a class="nav-link" href="#Spf" >SPF</a></li>
+      <li> <a class="nav-link" href="#Exfoliant" >Exfoliant</a></li>
+   </ul>
+</nav>
+<main id="main-doc">
+   <a onclick="backToTop()" id="top-btn">
+   <img src="https://img.icons8.com/android/24/000000/double-up.png"/></a>
+   <section id="Introduction" class="main-section  m-padding">
+      <header>Introduction</header>
+      <p>Skin is one of the largest organs of the body. Because of this, caring for your skin can directly affect your overall health. Your skin acts as a protective shield and is most vulnerable to outside elements. It’s affected by more factors than you may think. For instance, the following can play a role in your overall skin health</p>
+      <ul>
+         <li>Exposure to UV radiation in tanning beds</li>
+         <li>Exposure to chemical toxins in tobacco</li>
+         <li>Unprotected sun exposure for long periods of time</li>
+         <li>Not getting enough rest, fluids, or nutrition</li>
+      </ul>
+      <p>
+         There are steps you can take to ensure you have healthy skin. They include the following:
+      </p>
+      <ul>
+         <li>Cleanse regularly, typically twice daily</li>
+         <li>Apply a toner after cleansing if you have oily skin</li>
+         <li>Apply a moisturizer if you have dry skin</li>
+         <li>Exfoliate to remove dead skin cells and brighten up your complexion</li>
+      </ul>
+      <p>
+         Besides a daily skin care routine, make it a habit to examine your own skin for abnormalities, discolorations, or any other changes on a regular basis. Have your skin examined by a doctor or dermatologist annually for any changes, or if:
+      </p>
+      <ul>
+         <li>You have fair skin or many or large moles</li>
+         <li>You are in the sun or use tanning beds</li>
+         <li>You have a history of skin problems, irritations, or growths</li>
+      </ul>
+      <p>
+         It’s also important to protect your skin from too much sun and sun damage, which may increase wrinkles as well as lead to skin cancer. Cover your skin or use sunscreen to protect your skin from the damaging rays of the sun. See your doctor or dermatologist if any skin irritations or problems arise.
+      </p>
+   </section>
+   <section id="Oil_cleanser" class="main-section m-padding">
+      <header>Oil cleanser</header>
+      <p>
+         Oil cleansers can also be part of a multiple-step cleansing process to get the “best clean” you can. For example, makeup wearers like to cleanse with oil to remove product and dirt, followed by a foaming cleansing to soak up excess oil and remove anything left behind.
+      </p>
+      <code>
+         HOW TO APPLY <br>
+         <ol>
+            <li>Use one pump of oil cleanser</li>
+            <li>Soak a little facial towel in warm water and then press into your face. The heat from the towel will open your pores and make easier clean your skin. </li>
+            <li>Once your face is oily, use a damp towel to wipe the oil away. </li>
+            <li>Remove lipstick and eye makeup, including mascara, put a couple of drops of oil on damp cotton rounds </li>
+         </ol>
+      </code>
+   </section>
+   <section id="Foam_cleanser" class="main-section m-padding">
+      <header>Foam cleanser</header>
+      <p>
+         Foam cleansers create a foamy lather (hence the name), and are ideal for removing oil, layers of makeup and even hard-to-wash-off sunscreen. They can be a little bit harsh since they’re made to slightly dry out the skin, though some are formulated with oils or emollients to provide a more moisturizing experience.
+      </p>
+      <code>
+         HOW TO APPLY <br>
+         <ol>
+            <li>Wash your hands thoroughly. Put up your hair so it is out of your face.</li>
+            <li>Wet your face with water. Pump 2 or 3 pumps of foaming cleanser into the palms of your hands.</li>
+            <li>Rub your hands together to spread the cleanser onto your hands.</li>
+            <li> Apply the cleanser to your face by rubbing your hands along your entire face. Spread it to your forehead, along your cheekbones and nose, and on your chin.</li>
+            <li>Rub the cleanser gently into your skin in a circular motion with your fingertips.</li>
+            <li>Wash off the cleanser by splashing water onto your face. Dab your face dry with a clean towel.</li>
+         </ol>
+      </code>
+   </section>
+   <section id="Toner" class="main-section m-padding">
+      <header>Toner</header>
+      <p>
+         oners are often misunderstood and most people don't know if they should use it, or how to use it. Before moisturizing and following your facial cleanser, a face toner is a quick, absorbing liquid that helps to remove excess dirt, traces of oil and makeup, correct and balance the pH of your skin, and control acne. In other words, a facial toner thoroughly cleans the skin and helps remove built up surface dead skin cells. By helping to clean and close pores, it's especially beneficial for acne-prone skin.
+      </p>
+      <code>
+         HOW TO APPLY <br>
+         <ol>
+            <li> After cleansing, apply a small amount of liquid, alcohol-free facial toner onto a cotton ball or pad.</li>
+            <li>In small, gentle circles, apply the toner across your entire face.</li>
+            <li>Pay special attention to your hairline, chin, cheeks, and nose.</li>
+         </ol>
+      </code>
+   </section>
+   <section id="Essence" class="main-section m-padding">
+      <header>Essence</header>
+      <p>
+         Essences have similar benefits to a serum, and with a lower molecular weight than most daily creams, they work to penetrate the skin much deeper than your average moisturiser
+      </p>
+      <code>
+         HOW TO APPLY <br>
+         <ol>
+            <li>Pour about 2-3 drops of the essence into hand</li>
+            <li>Gently pat over face and neck for even coverage</li>
+         </ol>
+      </code>
+   </section>
+   <section id="Serum" class="main-section m-padding">
+      <header>Serum</header>
+      <p>
+         Serum is a skincare product you can apply to your skin after cleansing but before moisturizing with the intent of delivering powerful ingredients directly into the skin. Serum is particularly suited to this task because it is made up of smaller molecules that can penetrate deeply into the skin and deliver a very high concentration of active ingredients. This makes them a great tool for targeting specific skincare concerns, like wrinkles
+      </p>
+      <code>
+         HOW TO APPLY <br>
+         <ol>
+            <li> The amount you need may vary depending on your skin and the particular product you have.</li>
+            <li> The standard application is 2 pumps or a pea-sized amount.</li>
+            <li> If you have a dropper formula, use 3-4 drops of serum.</li>
+            <li> You want just enough to lightly cover (not coat) your entire face and neck.</li>
+         </ol>
+      </code>
+   </section>
+   <section id="Eye_cream" class="main-section  m-padding">
+      <header>
+      Eye cream</header
+      <p>
+         Eye cream will reduce signs of ageing as long as you apply your product  morning and night. Some eye cream help shadows and bags by increasing the circulation and others are better with fine lines and wrinkles. Some eye creams do both. Eye creams are generally thicker than face creams and target the lack of collagen production and sluggish circulation.
+      </p>
+      <code>
+         HOW TO APPLY <br>
+         <ol>
+            <li>Pick up a dime-sized amount of eye cream on your finger</li>
+            <li>Dot it on around the orbital bone</li>
+         </ol>
+      </code>
+   </section>
+   <section id="Moisturizer" class="main-section m-padding">
+      <header>Moisturizer</header>
+      <p>
+         A face moisturizer helps to prevent dryness and moisture loss in skin. A properly formulated moisturiser can deliver long-lasting hydration to help support skin’s natural barrier for protection against external aggressors. It is also an important step in prepping skin for makeup application. Face moisturisers are available in the form of serums, creams and facial masks.
+      </p>
+      <code>
+         <code>
+            HOW TO APPLY <br>
+            <ol>
+               <li> After you’ve waited 5 minutes for the serum to absorb, apply your moisturizer to your face and neck.</li>
+               <li> Wait 5 minutes for the mousturizer to absorbe before apply another product.</li>
+            </ol>
+         </code>
+      </code>
+   </section>
+   <section id="Spf" class="main-section m-padding">
+      <header>SPF</header>
+      <p>
+         SPF stands for Sun Protection Factor. It measures the level of protection a product will give you from UVB. The British Association of Dermatologists recommend always using a minimum of SPF 30, which blocks 97% of UVB. To put that into context, SPF 15 blocks 93% and SPF 50 blocks 98%.
+      </p>
+      <code>
+         HOW TO APPLY <br>
+         <ol>
+            <li>Apply sunscreen about 15 minutes before you go outside</li>
+            <li>Apply about a nickel-sized amount every day — even when it’s cloudy.</li>
+         </ol>
+      </code>
+   </section>
+   <section id="Exfoliant" class="main-section m-padding">
+      <header>Exfoliant</header>
+      <p>
+         Exfoliating is the process of removing dead skin cells from the surface of your skin using a chemical, granular substance, or exfoliation tool
+         Exfoliating can improve the appearance of your skin in several ways.
+         There are a few abrasive materials to choose from for manual exfoliation, including:
+      <ul>
+         <li>Cleansing scrubs</li>
+         <li>Exfoliating mitts</li>
+         <li>Dry brushes</li>
+         <li>Loofahs</li>
+         <li>Pumice stones</li>
+         <li>Microneedling or micro derma rollers</li>
+      </ul>
+      </p>
+   </section>
+</main>
+<!-- partial -->
+  <script  src="js/script-wiki.js"></script>
+
+</body>
+</html>
