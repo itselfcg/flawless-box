ea521181192c1a7d6bbe4aec0d5615c84ed0f0ad
Itzel Contreras <itselfcg@gmail.com>
2021-03-05 15:33:36 -0800

    Browsersync configuration.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,16 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+
+mix.browserSync({
+    proxy: 'http://localhost:8000/',
+    files: [
+        './app/**/*',
+        './routes/**/*',
+        './public/css/*.css',
+        './public/js/*.js',
+        './resources/views/**/*.blade.php',
+        './resources/lang/**/*',
+    ]
+})
