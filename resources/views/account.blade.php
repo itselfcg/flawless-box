09a1634da7d915c430ab35f6a7c21407cdb69d4a
Itzel Contreras <itselfcg@gmail.com>
2021-03-06 21:36:43 -0800

    Login page fixes to improve de resposive design.

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -18,83 +18,70 @@
 
 <main>
     <section id="header">
+        <div class="container">
+            <div class="row">
+                <div id="banner" class="col-md-6 col-sm-12 order-last order-md-first bg-lightblue">
+                    <div class="row align-items-center h-100">
+                        <div class="col-12 ">
 
-        <div class="row">
-            <div id="banner" class="col-md-6 col-sm-12 order-last order-md-first">
+                            <div class="text-center">
+                                <div class="title ">
 
-                <div class="h-50 row align-items-center">
-                    <div class="col">
-                        <div class=" container">
+                                    <h2>
+                                        CHECKOUT
 
-                            <div class="row text-center">
-                                <div class="col-lg-12 ">
-
-                                    <div class="title ">
-
-                                        <h2>
-                                            CHECKOUT
-
-                                            <div class="georgia"
-                                                 style="font-size: 2.5em; color: var(--lightblack)">
-                                                March's
-                                            </div>
+                                        <div class="georgia"
+                                             style="font-size: 2.5em; color: var(--mblack)">
+                                            March's
+                                        </div>
 
 
-                                            BRAND
-                                        </h2>
+                                        BRAND
+                                    </h2>
 
-                                    </div>
-
-                                    <a class="btn btn-orange text-light " href="wiki">
-                                        TAKE A LOOK
-                                    </a>
                                 </div>
+                                <a class="btn btn-orange text-light " href="wiki">
+                                    TAKE A LOOK
+                                </a>
                             </div>
-
-
                         </div>
-
                     </div>
                 </div>
 
-
-            </div>
-            <div class="col-md-6 col-sm-12 order-first order-md-last pt-5 pb-5 ">
-                <div class="h-100 row align-items-center">
-                    <div class=" container">
-
-                    <div class="col-md-12 col-lg-10 col-xl-7 offset-lg-1">
-
-                            <h1>Login</h1>
-                            <p>
-                               To keep connected with us please
-                                login with your personal info.
-                            </p>
-
-
-                            <form>
-                                <div class="form-group row">
-                                    <div class="col-sm-12">
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address">
+                <div id="login" class="col-md-6 col-sm-12 order-first order-md-last bg-lightgray">
+                    <div class="row align-items-center h-100">
+                        <div class="col-10 offset-1 ">
+                            <div>
+                                <h1>Login</h1>
+                                <p>
+                                    To keep connected with us please
+                                    login with your personal info.
+                                </p>
+                                <form>
+                                    <div class="form-group row">
+                                        <div class="col-sm-12">
+                                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                                   aria-describedby="emailHelp" placeholder="Email address">
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-sm-12">
-                                        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                                    <div class="form-group row">
+                                        <div class="col-sm-12">
+                                            <input type="password" class="form-control" id="inputPassword"
+                                                   placeholder="Password">
+                                        </div>
                                     </div>
-                                </div>
-                            </form>
-                            <p class="text-right">
-                                <a href="reset">Forgot password?</a>
-                            </p>
-                            <a class="btn btn-green" href="login">LOGIN</a> or <a href="subscription#packages"> Join now</a>
+                                </form>
+                                <p class="text-right">
+                                    <a href="reset">Forgot password?</a>
+                                </p>
+                                <a class="btn btn-green" href="login">LOGIN</a> or <a href="subscription#packages"> Join
+                                    now</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-
     </section>
 
 
