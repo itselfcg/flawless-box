4c13fcf0e7bccad80357f731909cb40633e8be29
Itzel Contreras <itselfcg@gmail.com>
2021-03-17 10:59:27 -0700

    Fix>: Ref to local js file.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -90,7 +90,7 @@
 </footer>
 
 <script src="{{ asset('js/app.js') }}" defer></script>
-<script src="/js/script.js" type="text/javascript"></script>
+<script src="{{ asset('js/script.js') }}" defer type="text/javascript"></script>
 
 
 </body>
