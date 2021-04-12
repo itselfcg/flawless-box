8bdfb0992034e83e13dcdd2812ca2c77fa267c60
Itzel Contreras <itselfcg@gmail.com>
2021-04-12 13:13:11 -0700

    Change: Forgot password, reset password and login pages were restructured to use a layout with all the basic info.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,127 +1,75 @@
-@extends('layout.master')
+@extends('layout.login')
 
-@section('title')
-    Login
-@stop
-
-@section('css-scripts')
-    <link rel="stylesheet" href="css/pages/login.css">
-@stop
-
-@section('content')
-    <section id="login">
-        <div class="row">
-            <div id="banner" class="col-md-6 col-sm-12 order-last order-md-first">
-                <div class="row align-items-center h-100">
-                    <div class="col-12 ">
-
-                        <div class="text-center">
-                            <div class="title">
-
-                                <div class="brand-title">
-                                    <h2>
-                                        CHECKOUT <br>
-                                        SPRING'S<br>
-                                        BOX<br>
-                                    </h2>
-
-                                    <a class="btn btn-orange w-100" href="subscription">
-                                        TAKE A LOOK
-                                    </a>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div id="login" class="col-md-6 col-sm-12 order-first order-md-last ">
-                <div class="container h-100">
-
-                    <div class="row h-100 align-items-center">
-                        <div class="col-lg-8 col-md-9 col-10 offset-1 ">
-                            <div>
-                                <h1>Login</h1>
-                                <p>
-                                    To keep connected with us please
-                                    login with your personal info.
-                                </p>
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                    <div class="form-group row">
-                                        <div class="col-sm-12">
-                                            <input id="email" type="email" placeholder="Email address"
-                                                   class="form-control @error('email') is-invalid @enderror"
-                                                   name="email" value="{{ old('email') }}" required autocomplete="email"
-                                                   autofocus>
-                                            @error('email')
-                                            <span class="invalid-feedback" role="alert">
+@section('title','Login')
+@section('header','Login')
+@section('description')
+    To keep connected with us please
+    login with your personal info.
+@endsection
+@section('form')
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="form-group row">
+            <div class="col-sm-12">
+                <input id="email" type="email" placeholder="Email address"
+                       class="form-control @error('email') is-invalid @enderror"
+                       name="email" value="{{ old('email') }}" required autocomplete="email"
+                       autofocus>
+                @error('email')
+                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <div class="col-sm-12">
-                                            <input id="password"
-                                                   type="password" placeholder="Password"
-                                                   class="form-control
-@                                                  @error('password') is-invalid @enderror"
-                                                   name="password" required autocomplete="current-password">
-
-                                            @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <div class="col-6 col-sm-5 ">
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" name="remember"
-                                                       id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                                <label class="form-check-label" for="remember">
-                                                    {{ __('Remember me') }}
-                                                </label>
-                                            </div>
-                                        </div>
-                                        <div class="col-6 col-sm-7 ">
-                                            <p class="text-right">
-
-                                                @if (Route::has('password.request'))
-                                                    <a href="{{ route('password.request') }}">
-                                                        {{ __('Forgot your password?') }}
-                                                    </a>
-                                                @endif
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group row mb-0">
-                                        <div class="col-md-12">
-
-                                            <button type="submit" class="btn btn-login">
-                                                {{ __('Login') }}
-                                            </button>
-                                            or <a
-                                                href="box#join-now" class="link"> Join
-                                                now</a>
-                                        </div>
-                                    </div>
-                                </form>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @enderror
             </div>
         </div>
-    </section>
-@stop
+        <div class="form-group row">
+            <div class="col-sm-12">
+                <input id="password"
+                       type="password" placeholder="Password"
+                       class="form-control
+@                                                  @error('password') is-invalid @enderror"
+                       name="password" required autocomplete="current-password">
 
-@section('js-scripts')
-    <script src="{{ asset('js/login.js') }}" defer type="text/javascript"></script>
-@stop
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                @enderror
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-6 col-sm-5 ">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember"
+                           id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                    <label class="form-check-label" for="remember">
+                        {{ __('Remember me') }}
+                    </label>
+                </div>
+            </div>
+            <div class="col-6 col-sm-7 ">
+                <p class="text-right">
+
+                    @if (Route::has('password.request'))
+                        <a href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+                </p>
+            </div>
+        </div>
+
+        <div class="form-group row mb-0">
+            <div class="col-md-12">
+
+                <button type="submit" class="btn btn-login">
+                    {{ __('Login') }}
+                </button>
+                or <a
+                    href="box#join-now" class="link"> Join
+                    now</a>
+            </div>
+        </div>
+    </form>
+@endsection
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,40 @@
-@extends('layout.master')
-@section('title','Reset')
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@extends('layout.login')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('title','Forgot password')
+@section('header','Reset')
+@section('description')
+    Forgot your password? We'll send you a link to reset your password.
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+@endsection
+@section('form')
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <div class="form-group row">
+            <div class="col-sm-12">
+                <input id="email" type="email"
+                       class="form-control @error('email') is-invalid @enderror"
+                       name="email"
+                       placeholder="{{ __('E-Mail Address') }}"
+                       value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                @enderror
             </div>
         </div>
-    </div>
-</div>
+
+        <div class="form-group row mb-0">
+            <div class="col-md-12">
+                <button type="submit" class="btn btn-green">
+                    {{ __('Send Password Reset Link') }}
+                </button>
+                or <a
+                    href="{{asset('login')}}" class="link"> Login</a>
+            </div>
+        </div>
+    </form>
 @endsection
+
